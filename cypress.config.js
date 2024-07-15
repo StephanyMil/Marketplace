f3641080dad362b4324303ec4d892e96665f515b
Stephany Milhomem <stephanymilhomem25@gmail.com>
2024-07-15 08:20:14 -0300

    ✨ Add coverage
    #

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "cypress";
+import codeCoverage from '@cypress/code-coverage/task.js';
 
 export default defineConfig({
   chromeWebSecurity: false,
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Implement node event listeners here
+      codeCoverage(on, config);
+      // It's important to return the updated config object
+      return config;
     },
     "baseUrl": "http://localhost:3000",
     testIsolation: false,
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import istanbul from 'vite-plugin-istanbul';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    istanbul({
+      include: 'src/**', // Instrument files within the src directory
+      exclude: ['node_modules', 'cypress/**'], // Exclude these directories
+      extension: ['.js', '.ts', '.jsx', '.tsx'], // File extensions to include
+    }),
+  ],
   server: {
     port: 3000,
   },
