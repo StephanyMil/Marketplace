9c9676465bf95d963d03a0afc40886377a54dbc0
Stephany Milhomem <stephanymilhomem25@gmail.com>
2024-07-19 09:18:38 -0300

    ✅ Improving branch coverage
    #

--- cypress/e2e/App.cy.jsx
+++ cypress/e2e/App.cy.jsx
@@ -143,66 +143,121 @@ describe('Marketplace', () => {
       cy.get(':nth-child(4) > a').contains('test').should('to.have.length', 1)
     })
   })
-  describe('Form Validation', () => {
-    it('Should show error messages when form fields are empty', () => {
-      cy.visit('/deployment/create/wordpress')
-      cy.contains('Create my wordpress').click()
-      cy.contains('Please fill in the required information to create the service.').should('to.have.length', 1)
-    })
+  describe('CreateService Component', () => {
+    const serviceType = 'wordpress';
+    const serviceDetails = {
+      uuid: '1234',
+      service: 'wordpress',
+      subdomain: 'test',
+      user: 'Testing',
+      status: '0',
+      url: 'test.opendata.center',
+    };
+  
+    const services = [serviceDetails];
+  
+    beforeEach(() => {
+      localStorage.setItem('services', JSON.stringify(services));
+    });
 
-    it('Should submit the form when fields are correctly filled', () => {
-      cy.visit('/deployment/create/wordpress')
-      cy.get('#subdomain').type('test')
-      cy.get('#user').type('Testing')
-      cy.get('#password').type('123456')
-      cy.get('#passwordConfirmation').type('123456')
-      cy.contains('Create my wordpress').click()
-      cy.url().should('include', '/deployment/details/')
-    })
-  })
+    describe('Form Validation', () => {
+      it('Should show error messages when form fields are empty', () => {
+        cy.visit('/deployment/create/wordpress')
+        cy.contains('Create my wordpress').click()
+        cy.contains('Please fill in the required information to create the service.').should('to.have.length', 1)
+      })
 
-  describe('LocalStorage Operations', () => {
-    it('Should save new service to localStorage', () => {
-      cy.visit('/deployment/create/wordpress')
-      cy.get('#subdomain').type('test')
-      cy.get('#user').type('Testing')
-      cy.get('#password').type('123456')
-      cy.get('#passwordConfirmation').type('123456')
-      cy.contains('Create my wordpress').click()
-      cy.wait(500) // Wait for the navigation to happen
-      cy.url().should('include', '/deployment/details/')
-      cy.window().then((win) => {
-        const services = JSON.parse(win.localStorage.getItem('services') || '[]')
-        expect(services).to.have.length(3)
-        expect(services[0].subdomain).to.equal('test')
+      it('Should submit the form when fields are correctly filled', () => {
+        cy.visit('/deployment/create/wordpress')
+        cy.get('#subdomain').type('test')
+        cy.get('#user').type('Testing')
+        cy.get('#password').type('123456')
+        cy.get('#passwordConfirmation').type('123456')
+        cy.contains('Create my wordpress').click()
+        cy.url().should('include', '/deployment/details/')
       })
     })
 
-    it('Should retrieve services from localStorage', () => {
-      cy.window().then((win) => {
-        win.localStorage.setItem('services', JSON.stringify([{ uuid: '1234', service: 'wordpress', status: '0', url: 'test.opendata.center' }]))
+    describe('LocalStorage Operations', () => {
+      it('Should save new service to localStorage', () => {
+        cy.visit('/deployment/create/wordpress')
+        cy.get('#subdomain').type('test')
+        cy.get('#user').type('Testing')
+        cy.get('#password').type('123456')
+        cy.get('#passwordConfirmation').type('123456')
+        cy.contains('Create my wordpress').click()
+        cy.wait(500) // Wait for the navigation to happen
+        cy.url().should('include', '/deployment/details/')
+        cy.window().then((win) => {
+          const services = JSON.parse(win.localStorage.getItem('services') || '[]')
+          expect(services).to.have.length(2)
+          expect(services[0].subdomain).to.equal('test')
+        })
       })
-      cy.visit('/deployment')
-      cy.contains('test.opendata.center').should('to.have.length', 1)
+
+      it('Should retrieve services from localStorage', () => {
+        cy.window().then((win) => {
+          win.localStorage.setItem('services', JSON.stringify([{ uuid: '1234', service: 'wordpress', status: '0', url: 'test.opendata.center' }]))
+        })
+        cy.visit('/deployment')
+        cy.contains('test.opendata.center').should('to.have.length', 1)
+      })
+    })
+
+    describe('Navigation', () => {
+      it('Should navigate to service details after creating a service', () => {
+        cy.visit('/deployment/create/wordpress')
+        cy.get('#subdomain').type('test')
+        cy.get('#user').type('Testing')
+        cy.get('#password').type('123456')
+        cy.get('#passwordConfirmation').type('123456')
+        cy.contains('Create my wordpress').click()
+        cy.url().should('include', '/deployment/details/')
+        cy.contains('Instance:').should('to.have.length', 1)
+      })
+
+      it('Should display service not found for invalid uuid', () => {
+        cy.visit('/deployment/details/invalid-uuid')
+        cy.contains('Service not found').should('to.have.length', 1)
+      })
     })
   })
-
-  describe('Navigation', () => {
-    it('Should navigate to service details after creating a service', () => {
-      cy.visit('/deployment/create/wordpress')
-      cy.get('#subdomain').type('test')
-      cy.get('#user').type('Testing')
-      cy.get('#password').type('123456')
-      cy.get('#passwordConfirmation').type('123456')
-      cy.contains('Create my wordpress').click()
-      cy.url().should('include', '/deployment/details/')
-      cy.contains('Instance:').should('to.have.length', 1)
-    })
-
-    it('Should display service not found for invalid uuid', () => {
-      cy.visit('/deployment/details/invalid-uuid')
-      cy.contains('Service not found').should('to.have.length', 1)
-    })
-  })
-
+  describe('ServiceDetails Component', () => {
+    const serviceDetails = {
+      uuid: '1234',
+      service: 'wordpress',
+      subdomain: 'test',
+      user: 'Testing',
+      status: '0',
+      url: 'test.opendata.center',
+    };
+  
+    const services = [serviceDetails];
+  
+    beforeEach(() => {
+      localStorage.setItem('services', JSON.stringify(services));
+    });
+  
+    it('Should display service details when service is found', () => {
+      cy.visit('/deployment/details/1234');
+      cy.contains('Create service wordpress').should('to.have.length', 1);
+      cy.contains('Instance: 1234').should('to.have.length', 1);
+      cy.contains('Cluster status').should('to.have.length', 1);
+      cy.contains('Service logs').should('to.have.length', 1);
+      cy.contains('Details').should('to.have.length', 1);
+      cy.get('.align-items-start > :nth-child(2) > .row > :nth-child(2)').contains('wordpress').should('to.have.length', 1);
+      cy.get(':nth-child(4) > a').contains('test').should('to.have.length', 1);
+    });
+  
+    it('Should display service not found message when service is not found', () => {
+      cy.visit('/deployment/details/invalid-uuid');
+      cy.contains('Service not found').should('to.have.length', 1);
+    });
+  
+    it('Should display correct service URL', () => {
+      cy.visit('/deployment/details/1234');
+      cy.get('a').contains('test.opendata.center').should('to.have.length', 1);
+    });
+  });
+  
 })
